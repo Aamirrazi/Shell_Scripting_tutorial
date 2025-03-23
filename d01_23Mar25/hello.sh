3fdd5ff2ba4efaeb0796cee45bb94f560951d496
Ubuntu <ubuntu@ip-172-31-43-124.us-east-2.compute.internal>
2025-03-23 19:55:54 +0000

    day1

---
+++ d01_23Mar25/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is a script
+
+echo "t:Hello everyone"
+echo "l:comment"
+echo "t:another comment"
---
+++ d01_23Mar25/shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<< comment
+ multiline comments
+comment
+
+name="aamir razi"
+
+echo "name is $name, date is $(date)"
+echo "enter the name:"
+read uname
+echo " you entered $uname"
